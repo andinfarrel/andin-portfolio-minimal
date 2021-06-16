b4c5d464c2c82ad8bc156b869761807d74de468d
Andin Farrel <andinfarrel@gmail.com>
2021-06-16 15:16:35 +0100

    fixed purge issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {

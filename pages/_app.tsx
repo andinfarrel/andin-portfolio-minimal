05acc6d1e60d48d8f478615cb521ee35221ccf1d
Andin Farrel <andinfarrel@gmail.com>
2021-06-16 14:51:57 +0100

    fix attempt no2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import NavBar from '../components/NavBar';
-import "tailwindcss/tailwind.css"
+// import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
   return (
